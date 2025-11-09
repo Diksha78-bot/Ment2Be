b8f4abf9274c2887ba49d29cda894b69ceabdb1a
Arsh Chouhan <arshchouhan004@gmail.com>
2025-11-09 21:05:01 +0530

    Web rtc implemented.. to be integrated with backend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,20 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import {Routes,Route} from 'react-router-dom'
+import './index.css'
+import Home from './pages/Home'
+import RoomPage from './pages/RoomPage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Hello</h1>
+       <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path="/room/:roomId"  element={<RoomPage/>}></Route>
+       </Routes>
     </>
   )
 }
