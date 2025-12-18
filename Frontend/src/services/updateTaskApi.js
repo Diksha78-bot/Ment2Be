8a1f7e6f2fd2d2e276abdcbb9401966eb66b935f
Arsh Chouhan <arshchouhan004@gmail.com>
2025-12-18 11:25:02 +0530

    Minor Update

--- Frontend/src/services/updateTaskApi.js
+++ Frontend/src/services/updateTaskApi.js
@@ -5,7 +5,7 @@
  */
 
 // Switch between 'nodejs', 'java', or 'render'
-const ACTIVE_BACKEND = 'java';
+const ACTIVE_BACKEND = 'render';
 
 const BACKEND_URLS = {
   nodejs: 'http://localhost:4000',
